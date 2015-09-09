3c7801fa4d2711f19c67be9eeabeb8b87d4b50ad
Rachel Morgan <rachyllmorgan@gmail.com>
2015-09-09 14:34:15 -0500

    cleaning and notes added

--- music.js
+++ music.js
@@ -1,64 +1,35 @@
-// var songs = [];
-// var list_of_songs = document.getElementById("songlist");
-
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-
-// var addSongBottom = songs.push("Hold Back the River > by James Bay on the album Chaos and the Calm");
-// var addSongTop = songs.unshift("Just One Drink > by Jack White on the album Lazaretto");
-
-//var addSong = prompt("Pick a song");
-//	if (addSong===""); {
-//var addArtist = prompt("Who sings it?")
-//	}
-//	if (addSong===""); {
-//var addAlbum = prompt("On what album?")
-//	songs.push(addSong + " &#8208 by " + addArtist + " on the album " + addAlbum);
-//}
-
-// console.log(song_mod);
-// list_of_songs.innerHTML += "<div class='list_mod'>" + song_mod + "</div>" + "<br>";
-// }
-
-//$(document).ready(function(){	
-//	
-//		$.ajax({
-//				url: "music.json",
-//		}).done(function(data) {
-
-//});
-
-
 $(document).ready(function(){	
-	
+
+	// Load songs from JSON
 		$.ajax({
-				url: "music.json",
+			url: "music.json",
 		}).done(function(data) {
 			var table = '';
-		for(var i =0; i <= data.songs.length; i++) {
-			console.log(data.songs[i].name);
-			table += "<table><tr><h3>"+data.songs[i].name+"</h3></tr>"+"<tr>"+data.songs[i].artist+"</tr></br>"+"<tr>"+data.songs[i].album+"</tr></table>";
-			$('#table-body').html(table);
-		}
-						})
+			for(var i =0; i <= data.songs.length; i++) {
+				console.log("data.songs", data.songs);
+				table += "<table><tr><h3>"+ data.songs[i].name +"</h3></tr>"+"<tr>"+data.songs[i].artist+"</tr></br>"+"<tr>"+data.songs[i].album+"</tr></table>"+"<input type='button' value='Remove' class='remove'>";
+				$('#table-body').html(table);
+			
+			}
+		})
+		
+		// Add additional JSON file
 		$(".more").click(function(){
 			$.ajax({
 				url: "music2.json",
 			}).done(function(data) {
-			var table = '';
-		for(var i =0; i <= data.songs.length; i++) {
-			console.log(data.songs[i].name);
-			table += "<table><tr><h3>"+data.songs[i].name+"</h3></tr>"+"<tr>"+data.songs[i].artist+"</tr></br>"+"<tr>"+data.songs[i].album+"</tr></table>";
-			$(".remove").click(function() {
-				$("tr").remove("<tr>");
+				var table = '';
+				for(var i =0; i <= data.songs.length; i++) {
+					console.log("data.songs[i].name", data.songs[i].name);
+					table += "<table><tr><h3>"+data.songs[i].name+"</h3></tr>"+"<tr>"+data.songs[i].artist+"</tr></br>"+"<tr>"+data.songs[i].album+"</tr>"+"<input type='button' value='Remove' class='remove'></table>";
+				}
+				$('#table-body').append(table);
 			})
-			$('#table-body').html(table);
-		}
-						})
 		})
-		
+
+		// Remove songs
+		$(document).on("click", ".remove", function(){
+			console.log("Remove Clicked");
+			$(this).prev().remove();
+		})
 });
